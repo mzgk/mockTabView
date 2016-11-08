d7695628983151029a47c0d33aa1d273707cb67e
takeshi mizugaki <takeshi.mizugaki@ast-net.co.jp>
2016-11-08 14:11:55 +0900

    Initial Commit

---
+++ mockTabCollectionView/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  mockTabCollectionView
+//
+//  Created by mzgk on 2016/11/08.
+//  Copyright © 2016年 mzgk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
